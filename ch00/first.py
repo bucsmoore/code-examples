bfa9db579235f8cda85b6e2159823e46f093140b
slgy <me@nowhere.com>
2023-08-28 11:04:14 -0400

    updated

--- ch00/first.py
+++ ch00/first.py
@@ -2,16 +2,31 @@
 # After each color, it waits 1000 milliseconds before continuing.
 import pygame
 
+# Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode()
-color = [0, 234, 255]
-screen.fill(color)
+background = pygame.display.set_mode()
+
+# Set the color of the background to red and wait 1 second
+color = (255, 0, 0)
+background.fill(color)
 pygame.display.flip()
+
 pygame.time.wait(1000)
-color = [78, 132, 97]
-screen.fill(color)
+
+# Set the color of the background to green and wait 1 second
+color = (0, 255, 0)
+background.fill(color)
+
 pygame.display.flip()
+
 pygame.time.wait(1000)
-color = [78, 1, 176]
-screen.fill(color)
+
+# Set the color of the background to blue and wait 1 second
+color = (0, 0, 255)
+background.fill(color)
 pygame.display.flip()
+
+pygame.time.wait(1000)
+
+pygame.quit()
+quit()

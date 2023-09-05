b9cf67ced06f3959ee5d2c83bb7ff9a87b3e741d
slgy <me@nowhere.com>
2023-09-05 16:20:32 -0400

    updated

--- ch00/main.py
+++ ch00/main.py
@@ -6,3 +6,72 @@
 # Machine Code : 000011100111011
 # Operating System
 # Shell
+
+# Algorithm
+
+# - x + x
+# - cookie recipe
+# - Solving rubik's cube
+
+# RULE: A valid algorithm with valid inputs ALWAYS PRODUCES valid output
+
+# ## Data
+
+# - binary values stored in memory
+# - user input (must be converted to binary and stored in memory))
+# - from another computer
+
+# Data vs Algorithm
+
+## Algorithms of a Vending Machine
+# - compare price to money inserted
+# - finding the location of the item
+# - calculate change if necessary
+# - dispensing the item
+
+# ## Data of a VM
+
+# - prices of items
+# - code of the item
+# - number of items avilable
+# - Money in the system
+# - Amount of money entered and change
+# - input code
+
+
+# Python
+
+# - Java
+# - C/C++
+# - Python
+# - Javascript # not Java
+
+
+# REPL
+# - Retrieve
+# - Execute
+# - Process
+# - Loop
+
+# Command vs Script vs Program
+
+# - Command: Package algorithm takes input and produces a *single* result
+# - Script: File that contains a list of commands to be run in sequence
+# - Program: a script with data permanence
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode()
+# [red, green, blue]
+color = [80, 200, 150]
+screen.fill(color)
+pygame.display.flip()
+pygame.time.wait(1000)
+color = [0, 255, 0]
+screen.fill(color)
+pygame.display.flip()
+pygame.time.wait(1000)
+color = [255, 0, 255]
+screen.fill(color)
+pygame.display.flip()
---
+++ ch00/notes.md
@@ -0,0 +1,12 @@
+# Repository
+
+- a folder on a computer that contains a project
+
+- Local Repository
+
+  - Pro: Simple
+  - Con: hard share, large projects get messy
+
+- Distributed Repository
+  - git
+    - NOT GOOGLE DOCS FOR CODE

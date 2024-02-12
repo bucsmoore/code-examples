f06da9d57c5e8a9318f65d947ed8629dabe6cc23
slgy <me@nowhere.com>
2024-02-12 14:30:54 -0500

    updated for spring 2024

--- ch01/main.py
+++ ch01/main.py
@@ -28,36 +28,104 @@ import turtle
 # myscreen.exitonclick()
 
 
-# for variable in list:
-#     algorithm
+# # for variable in list:
+# #     algorithm
 
-# for i in [1,2,3,4]:
-#     print(i)
-#     i = i + 10
-#     print(i)
+# # for i in [1,2,3,4]:
+# #     print(i)
+# #     i = i + 10
+# #     print(i)
+# # number_of_nums = 10
+# # print(list(range(number_of_nums)))#stop
+# # print(list(range(1, 10)))#start, stop
+# # print(list(range(1,10, 3))) #start, stop, step
 
-print(list(range(10)))#stop
-print(list(range(1, 10)))#start, stop
-print(list(range(1,10, 3))) #start, stop, step
 
+# myturtle = turtle.Turtle()
+# myscreen = turtle.Screen()
+# myturtle.shape("turtle")
+# myturtle.xcor = -200
 
-myturtle = turtle.Turtle()
-myscreen = turtle.Screen()
-myturtle.shape("turtle")
-myturtle.xcor = -200
+# colors = ["purple", "blue", "black", "yellow", "green"]
 
-colors = ["purple", "blue", "black", "yellow", "green"]
-
-for color in colors:
-    myturtle.down()
-    myturtle.color(color)
+# for color in colors:
+#     myturtle.down()
+#     myturtle.color(color)
     
-    for i in range(4): #[0]*4
-        random.sample("ABCDEF0123456789", k=6)
-        myturtle.forward(100)
-        myturtle.left(90)
+#     for i in range(4): #[0]*4
+#         myturtle.forward(100)
+#         myturtle.left(90)
         
-    myturtle.up()
-    myturtle.forward(15)
+#     myturtle.up()
+#     myturtle.forward(15)
 
-myscreen.exitonclick()
+# # myscreen.exitonclick()
+
+
+# # FOR LOOPs
+
+# mylist = ['a', 'b', 'c']
+# mystr = "abc"
+
+# for element in mylist:
+#     print(element)
+
+# for char in mystr:
+#     next_char = ord(char) + 1
+#     next_char = chr(next_char)
+#     print(next_char)
+
+# msg = "Happy Birthday!"
+
+# for _ in range(4): #[0,1,2,3]
+#     print(msg)
+
+
+# # For Loop
+
+# # 1. Loop through a list of objects and apply an algorithm to each one
+# # 2. Loop a specified number of times and perform an algorithm for each loop (i.e. iterations)
+
+# print( list(range(10)) )
+# print( list(range(5, 10)) )
+# print( list(range(5, 10, 5)) )
+
+# mylist = ['a', 'b', 'c', 'd']
+# for x in range(0, 4, 2):
+#     print(mylist[x])
+
+hex_chars = "ABCDEF0123456789"
+
+# color_str = "#"
+# for _ in range(6):
+#     color_str.append(random.choice(hex_chars))
+
+# list comprehension
+color_char_list = [random.choice(hex_chars) for _ in range(6)]
+
+color_str = "".join(color_char_list)
+
+color_str = "#"+color_str
+print(color_str)
+
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode()
+
+wsize = pygame.display.get_window_size()
+x_size = wsize[0]
+y_size = wsize[1]
+center_coor = [x_size/2, y_size/2]
+
+while True:  
+    # check for events since the last time through the loop
+    for event in pygame.event.get():
+        pass
+
+    #code 
+    
+    pygame.display.flip()
+    
+    break

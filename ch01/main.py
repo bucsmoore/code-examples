50782b22fd754b8186711c3ed95490f2e2a7d3de
slgy <me@nowhere.com>
2024-08-29 06:07:42 -0400

    huhu

---
+++ ch01/main.py
@@ -0,0 +1,39 @@
+# print("Hello")
+# print("Goodbye")
+# print()
+# print("Hello", "Goodbye", 1, 2)
+
+
+# print(chr(65), "65", ord("A"))
+
+# print(ord("1"), ord("A"), ord("a"))
+
+# print(type("1"), type(1), type(1.0))
+
+
+# Strings
+
+"Hello"
+
+"""
+Hello
+goodbye
+"""
+
+"Hello"
+
+"Bruce's Beard"
+'Knights who say, "Ni!"'
+"""
+'Knights who say, "Ni!"'
+"""
+"""
+
+'''
+'Knights who say, "Ni!"'
+"""
+
+# print(len("Test Text"))
+print("Test Text"[1])
+print("Test Text"[0])
+print("Test Text"[-2])
--- ch01/notes.md
+++ ch01/notes.md
@@ -1,6 +1,20 @@
 # Chapter 1: Data
 
-- Algorithms doen't change
+- Algorithms doesn't change
+
 - Data will change
 
-> RULE #1: Separate things that change from things that stay the same
+## RULE #1: Separate things that change from things that stay the same
+
+
+Algorithms end with parenthesis, sometimes with arguments
+
+Algorithms => Functions
+
+print()
+
+ALL data has a type and value
+
+1, int
+"1", str
+1.0, float

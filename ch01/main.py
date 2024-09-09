76e8f28d80caa62438dc49b33ffd12214123251d
slgy <me@nowhere.com>
2024-09-09 16:02:43 -0400

    huhu

--- ch01/main.py
+++ ch01/main.py
@@ -33,7 +33,85 @@ goodbye
 'Knights who say, "Ni!"'
 """
 
-# print(len("Test Text"))
-print("Test Text"[1])
-print("Test Text"[0])
-print("Test Text"[-2])
+# # print(len("Test Text"))
+# print("Test Text"[1])
+# print("Test Text"[0])
+# print("Test Text"[-2])
+
+# # Built-in Module Function
+# print(1, "Hello", "3")
+# print(type(1), type("1"))
+
+
+# print(5 + 3)
+# print(5 - 3)
+# print(5 * 3)
+# print(5 / 3)
+# print(5**3)
+# print(5 // 3)
+# print(5 % 3)  # most important, 5 /3 = ? R 2
+
+
+# print("5" + "3")
+# # print("5" - "3")  # error
+# print("5" * 3000)  # error
+# # print("5" / "3")  # error
+
+# # integer/ floating point
+
+# print(1, type(1))
+# print(1.0, type(1.0))
+
+# print(1.0 - 2)
+
+
+# print(10 * 5)  # Expected Answer: 50
+# print(10**2)  # Expected Answer: 100
+# print(15 / 10)  # Expected Answer: 1.5
+# print(15 // 10)  # Expected Answer: 1
+# print(-15 // 10)  # Expected Answer:-2
+# print(15 % 10)  # Expected Answer: 5
+# print(10 % 15)  # Expected Answer: 10
+# print(10 % 10)  # Expected Answer: 0
+# print(0 % 10)  # Expected Answer: 0
+# print(10 / 15)  # Expected Answer: 0.66666666
+
+# var = 6
+
+# print(5 * var)
+# print(10 * var)
+# print(11 * var)
+# print(type(var))
+# # assignment operator
+# var = "Hi"
+
+# print(var)
+
+# print(type(var))
+
+
+# my_list = [1, 2.0, "3"]
+# print(my_list)
+
+# my_other_list = []
+# print(my_other_list)
+# my_other_list.append("data")
+# print(my_other_list)
+# print(my_other_list[0])
+# print(my_other_list[-1])
+# list_length = len(my_other_list)
+# print(list_length)
+
+# my_desserts = ["raspberry pie", "choclate chip cookies", "ice cream"]
+# dessert = my_desserts[1]
+# print(dessert)
+# print(dessert[4])
+# print(my_desserts[1][4])
+
+
+result = input("Please enter something: ")
+print(result, type(result))
+
+# result = int(result)
+
+# print(result * 2)
--- ch01/notes.md
+++ ch01/notes.md
@@ -18,3 +18,48 @@ ALL data has a type and value
 1, int
 "1", str
 1.0, float
+
+
+# Review: Data Type
+
+- Data
+  - Type: int
+  - Value: 64
+  - Type: str
+  - Value: 'A' (64)
+
+  Duck Type Language:
+
+  1 (int)
+  1 (str)
+
+
+
+
+# Rule #2
+
+- Don't Repeat Yourself
+  - Keep your code DRY
+
+## Variables
+
+- A memory bucket to hold data
+- It's a named address
+
+- Variable names
+  - must start with a letter
+    - should be lowercase
+    - spaces respresent with a _, my_var
+    - letters, numbers, and underscores
+    - reserved words:
+      - `class, True, for`
+
+
+- int
+- float
+- str
+
+- container types
+  - list
+    - contains a list of data objects
+    - they do not have to be the same type
